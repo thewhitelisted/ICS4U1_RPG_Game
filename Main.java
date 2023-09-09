a72c0a957680696796635a82254b699adf905678
Christopher Lee <clee2k6@outlook.com>
2023-09-09 15:08:44 -0400

    changed variables to fit cadawas style

--- Main.java
+++ Main.java
@@ -17,20 +17,20 @@ public class Main {
 		Console con = new Console(1000,600);
 		
 		// map and textures, see function render_map()
-		String map[][] = load_map("map.csv");
-		BufferedImage grass = con.loadImage("grass.png");
-		BufferedImage tree = con.loadImage("tree.png");
-		BufferedImage water = con.loadImage("water.png");
-		BufferedImage none = con.loadImage("none.png");
-		BufferedImage player = con.loadImage("character.png");
+		String[][] strMap = load_map("map.csv");
+		BufferedImage imgGrass = con.loadImage("grass.png");
+		BufferedImage imgTree = con.loadImage("tree.png");
+		BufferedImage imgWater = con.loadImage("water.png");
+		BufferedImage imgNone = con.loadImage("none.png");
+		BufferedImage imgPlayer = con.loadImage("character.png");
 		
 		// start screen
 		start_menu(con);
 		
 		// main loop
 		while (true) {
-			render_map(con, map, grass, tree, water, none);
-			con.drawImage(player, 120, 120);
+			render_map(con, strMap, imgGrass, imgTree, imgWater, imgNone);
+			con.drawImage(imgPlayer, 120, 120);
 			con.repaint();
 		}
 	}
@@ -44,34 +44,34 @@ public class Main {
 	}
 	
 	// function to load a csv file into a 2x2 array of length 20
-	public static String[][] load_map(String map_name) {
-		TextInputFile map = new TextInputFile(map_name);
-		String loaded_map[][] = new String[20][20];
-		String map_line[];
+	public static String[][] load_map(String strMap_name) {
+		TextInputFile txtMap = new TextInputFile(strMap_name);
+		String strLoaded_map[][] = new String[20][20];
+		String strMap_line[];
 		
 		// read map to variable
 		for (int i = 0; i < 20; i++) {
-			map_line = map.readLine().split(",");
+			strMap_line = txtMap.readLine().split(",");
 			for (int j = 0; j < 20; j++) {
-				loaded_map[i][j] = map_line[j];
+				strLoaded_map[i][j] = strMap_line[j];
 			}
 		}
-		return loaded_map;
+		return strLoaded_map;
 	}
 	
 	// function to render the map
-	public static void render_map(Console con, String[][] map, BufferedImage grass, BufferedImage tree, BufferedImage water, BufferedImage none) {
+	public static void render_map(Console con, String[][] strMap, BufferedImage imgGrass, BufferedImage imgTree, BufferedImage imgWater, BufferedImage imgNone) {
 		// render code, cool wonky advanced stuff
 		for (int i = 0; i < 20; i++) {
 			for (int j = 0; j < 20; j++) {
-				if (map[i][j].equals("g")) {
-					con.drawImage(grass, j*30, i*30);
-				} else if (map[i][j].equals("t")) {
-					con.drawImage(tree, j*30, i*30);
-				} else if (map[i][j].equals("w")) {
-					con.drawImage(water, j*30, i*30);
+				if (strMap[i][j].equals("g")) {
+					con.drawImage(imgGrass, j*30, i*30);
+				} else if (strMap[i][j].equals("t")) {
+					con.drawImage(imgTree, j*30, i*30);
+				} else if (strMap[i][j].equals("w")) {
+					con.drawImage(imgWater, j*30, i*30);
 				} else {
-					con.drawImage(none, j*30, i*30);
+					con.drawImage(imgNone, j*30, i*30);
 				}
 			}
 		}
